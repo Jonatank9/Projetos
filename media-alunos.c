a7628ac794c62359f4ce44e8e52f2ba209d8d7a8
Jonatan da Silva Schmidt <jonatank9@hotmail.com>
2025-09-30 11:07:46 +0000

    atualização com if e else

--- media-alunos.c
+++ media-alunos.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int nota1, nota2, nota3;
-    float media;
+    
+    float nota1, nota2, media;
 
-    printf("***Cálculo da Média dos Alunos***\n");
+    printf("Digite as notas dos alunos \n");
+    printf("Digite a primeira nota \n");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota \n");
+    scanf("%f", &nota2);
 
-    printf("Digite a primeira nota: \n");
-    scanf("%d", &nota1);
+    media = (nota1 + nota2) / 2;
+    printf("A media dos alunos é: %.2f\n", media);
 
-    printf("Digite a segunda nota: \n");
-    scanf("%d", &nota2);
-
-    printf("Digite a terceira nota: \n");
-    scanf("%d", &nota3);
-
-    // caso, nota1, nota2 e nota3 fossem do tipo float, não seria necessário o casting (float)
-    media = (float)(nota1 + nota2 + nota3) / 3;
-    printf("A média do aluno é: %.2f\n", media);
-
+    if (media >= 6) {
+        printf("Parabéns, você foi aprovado!!!\n");
+    } else {
+        printf("Infelizmente, você foi reprovado.\n");
+    }
 
 
     return 0;
